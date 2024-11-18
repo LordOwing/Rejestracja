fcc7702ef4849a502d137ceda916208b51b3721e
LordOwing <michallolo12345@gmail.com>
2024-11-18 08:09:09 +0100

    Initial commit

---
+++ app/src/main/java/com/example/rejestracja/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.rejestracja;
+
+import android.os.Bundle;
+
+
+
+
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        findViewById(R.id.send).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = ((EditText) findViewById(R.id.form1)).getText().toString().trim();
+
+
+                String pass = ((EditText) findViewById(R.id.form2)).getText().toString().trim();
+                String passConf = ((EditText) findViewById(R.id.form3)).getText().toString().trim();
+                TextView text_2 = findViewById(R.id.text_2);
+
+                if (email.isEmpty()) {
+                     text_2.setText("Podaj e-mail");
+                } else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    text_2.setText("Nieprawidłowy adres e-mail");
+                } else if (pass.isEmpty()) {
+                    text_2.setText("Podaj hasło");
+
+                } else if (passConf.isEmpty()) {
+                    text_2.setText("Powtórz hasło");
+                } else if (!pass.equals(passConf)) {
+                    text_2.setText("Hasła się różnią");
+                } else {
+                    text_2.setText("");
+
+                }
+            }
+        });
+    }
+}
